f0467cd68cfbf21a83756b06c0429f6981412e9a
Olivier Bourgault <77986350+Olivier1875009@users.noreply.github.com>
2023-09-13 15:28:29 -0400

    Update electrolib.sql

--- electrolib.sql
+++ electrolib.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Sep 12, 2023 at 03:35 PM
--- Server version: 8.1.0
--- PHP Version: 8.2.9
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mer. 13 sep. 2023 à 17:46
+-- Version du serveur : 8.0.31
+-- Version de PHP : 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,27 +18,27 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `electrolib`
+-- Base de données : `electrolib`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `authors`
+-- Structure de la table `authors`
 --
 
 DROP TABLE IF EXISTS `authors`;
 CREATE TABLE IF NOT EXISTS `authors` (
   `idAuthor` int NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `last_name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `firstName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`idAuthor`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `books`
+-- Structure de la table `books`
 --
 
 DROP TABLE IF EXISTS `books`;
@@ -46,29 +46,22 @@ CREATE TABLE IF NOT EXISTS `books` (
   `idBook` int NOT NULL AUTO_INCREMENT,
   `idGenre` int NOT NULL,
   `idAuthor` int NOT NULL,
-  `title` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `isbn` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `title` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `isbn` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `isBorrowed` tinyint(1) NOT NULL,
-  `cover` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cover` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `publishedDate` datetime NOT NULL,
-  `originalLanguage` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`idBook`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `books`
---
-
-INSERT INTO `books` (`idBook`, `idGenre`, `idAuthor`, `title`, `description`, `isbn`, `isBorrowed`, `cover`, `publishedDate`, `originalLanguage`) VALUES
-(1, 0, 0, 'Seigneur des anneaux', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean id gravida lectus. Fusce at iaculis leo, et volutpat arcu. Duis vehicula placerat tortor, ac blandit magna lacinia vitae. Quisque vulputate et metus eget faucibus. Suspendisse eleifend dui en', '9780140328721', 0, '8739161', '2023-09-09 17:56:57', 'EN'),
-(2, 0, 0, 'Harry Potter', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean id gravida lectus. Fusce at iaculis leo, et volutpat arcu. Duis vehicula placerat tortor, ac blandit magna lacinia vitae. Quisque vulputate et metus eget faucibus. Suspendisse eleifend dui en', '9780140328722', 0, '8739162', '2023-09-09 17:56:57', 'EN'),
-(3, 0, 0, 'Hunger Games', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean id gravida lectus. Fusce at iaculis leo, et volutpat arcu. Duis vehicula placerat tortor, ac blandit magna lacinia vitae. Quisque vulputate et metus eget faucibus. Suspendisse eleifend dui en', '9780140328723', 0, '8739163', '2023-09-09 17:56:57', 'EN');
+  `originalLanguage` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`idBook`),
+  KEY `idGenre` (`idGenre`,`idAuthor`),
+  KEY `idAuthor` (`idAuthor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `borrows`
+-- Structure de la table `borrows`
 --
 
 DROP TABLE IF EXISTS `borrows`;
@@ -79,13 +72,15 @@ CREATE TABLE IF NOT EXISTS `borrows` (
   `borrowedDate` datetime NOT NULL,
   `dueDate` datetime NOT NULL,
   `returnedDate` datetime NOT NULL,
-  PRIMARY KEY (`idBorrow`)
+  PRIMARY KEY (`idBorrow`),
+  KEY `idUser` (`idUser`,`idBook`),
+  KEY `idBook` (`idBook`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `evaluations`
+-- Structure de la table `evaluations`
 --
 
 DROP TABLE IF EXISTS `evaluations`;
@@ -94,13 +89,15 @@ CREATE TABLE IF NOT EXISTS `evaluations` (
   `idUser` int NOT NULL,
   `idBook` int NOT NULL,
   `score` int NOT NULL,
-  PRIMARY KEY (`idEvaluation`)
+  PRIMARY KEY (`idEvaluation`),
+  KEY `idUser` (`idUser`,`idBook`),
+  KEY `idBook` (`idBook`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `favorites`
+-- Structure de la table `favorites`
 --
 
 DROP TABLE IF EXISTS `favorites`;
@@ -109,26 +106,28 @@ CREATE TABLE IF NOT EXISTS `favorites` (
   `idUser` int NOT NULL,
   `idBook` int NOT NULL,
   `favoriteDate` datetime NOT NULL,
-  PRIMARY KEY (`idFavorite`)
+  PRIMARY KEY (`idFavorite`),
+  KEY `idUser` (`idUser`,`idBook`),
+  KEY `idBook` (`idBook`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `genres`
+-- Structure de la table `genres`
 --
 
 DROP TABLE IF EXISTS `genres`;
 CREATE TABLE IF NOT EXISTS `genres` (
   `idGenre` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`idGenre`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `reservations`
+-- Structure de la table `reservations`
 --
 
 DROP TABLE IF EXISTS `reservations`;
@@ -137,38 +136,77 @@ CREATE TABLE IF NOT EXISTS `reservations` (
   `idUser` int NOT NULL,
   `idBook` int NOT NULL,
   `reservationDate` datetime NOT NULL,
-  PRIMARY KEY (`idReservation`)
+  PRIMARY KEY (`idReservation`),
+  KEY `idUser` (`idUser`,`idBook`),
+  KEY `idBook` (`idBook`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Structure de la table `users`
 --
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `idUser` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(180) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `registrationDate` datetime NOT NULL,
-  `firstName` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `lastName` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `profilePicture` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `address` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `phoneNumber` varchar(12) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `postalCode` varchar(7) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `roles` json DEFAULT NULL,
+  `firstName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `lastName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `profilePicture` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `address` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phoneNumber` varchar(12) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `postalCode` varchar(7) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `roles` longtext COLLATE utf8mb4_unicode_ci,
   PRIMARY KEY (`idUser`),
   UNIQUE KEY `UNIQ_1483A5E9E7927C74` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Dumping data for table `users`
+-- Contraintes pour les tables déchargées
 --
 
-INSERT INTO `users` (`idUser`, `email`, `password`, `registrationDate`, `firstName`, `lastName`, `profilePicture`, `address`, `phoneNumber`, `postalCode`, `roles`) VALUES
-(1, 'user@electrolib.com', '1234', '2023-09-09 12:46:35', 'Olivier', 'Bourgault', 'assets\\images\\contact-book.png', '123 rue Allo', '1234567890', 'K2W0A9', '[\"ROLES_USER\"]');
+--
+-- Contraintes pour la table `authors`
+--
+ALTER TABLE `authors`
+  ADD CONSTRAINT `authors_ibfk_1` FOREIGN KEY (`idAuthor`) REFERENCES `books` (`idAuthor`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `borrows`
+--
+ALTER TABLE `borrows`
+  ADD CONSTRAINT `borrows_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `users` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `borrows_ibfk_2` FOREIGN KEY (`idBook`) REFERENCES `books` (`idBook`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `evaluations`
+--
+ALTER TABLE `evaluations`
+  ADD CONSTRAINT `evaluations_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `users` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `evaluations_ibfk_2` FOREIGN KEY (`idBook`) REFERENCES `books` (`idBook`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `favorites`
+--
+ALTER TABLE `favorites`
+  ADD CONSTRAINT `favorites_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `users` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `favorites_ibfk_2` FOREIGN KEY (`idBook`) REFERENCES `books` (`idBook`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `genres`
+--
+ALTER TABLE `genres`
+  ADD CONSTRAINT `genres_ibfk_1` FOREIGN KEY (`idGenre`) REFERENCES `books` (`idGenre`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `reservations`
+--
+ALTER TABLE `reservations`
+  ADD CONSTRAINT `reservations_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `users` (`idUser`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `reservations_ibfk_2` FOREIGN KEY (`idBook`) REFERENCES `books` (`idBook`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
