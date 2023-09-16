8f04f2de46e35c7f5150de9eba08231f20b4f332
Olivier Bourgault <77986350+Olivier1875009@users.noreply.github.com>
2023-09-16 17:31:28 -0400

    Update the book route
    Complete the route to recover all the books with criteria

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Doctrine\DBAL\Connection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 header('Access-Control-Allow-Origin: *');
@@ -15,9 +16,21 @@ class BookController extends AbstractController
     // Route to get all the books
     //--------------------------------
     #[Route('/books')]
-    public function getAll(Connection $connexion): JsonResponse
+    public function getAll(Request $request, Connection $connexion): JsonResponse
     {
-        $books = $connexion->fetchAllAssociative("SELECT * FROM books");
+        $idGenre = $request->query->get('idGenre');
+        //$search = $request->query->get('search');
+
+        if ($idGenre) {
+            $books = $connexion->fetchAllAssociative("SELECT * FROM books WHERE idGenre IN($idGenre)");
+        } else {
+            $books = $connexion->fetchAllAssociative("SELECT * FROM books");
+        }
+
+        // if($search) {
+        //     $books = $connexion->fetchAllAssociative("SELECT * FROM books WHERE title LIKE '%$search%'");
+        // }
+
         return $this->json($books);
     }
 }
