3b5a268f692e5f9c8b859879452e2409bed93b1d
1871862@cstj.qc.ca <72227583+Ethzac@users.noreply.github.com>
2023-09-24 20:29:26 -0400

    changement nom route getAll emprunts

--- src/Controller/BorrowController.php
+++ src/Controller/BorrowController.php
@@ -6,6 +6,7 @@ use Doctrine\DBAL\Connection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 header('Access-Control-Allow-Origin: *');
 
@@ -15,7 +16,7 @@ class BorrowController extends AbstractController
     // Route to get all the borrows
     //--------------------------------
     #[Route('/borrows')]
-    public function getAll(Connection $connexion): JsonResponse
+    public function getAllFromUser(Request $request, Connection $connexion): JsonResponse
     {
         $borrows = $connexion->fetchAllAssociative("SELECT * FROM borrows");
         return $this->json($borrows);
