f7a54317249331df0fed1e30ae5e00609e1d1dcd
VincentLavoieWhitlock <89476781+VincentLavoieWhitlock@users.noreply.github.com>
2023-11-12 10:23:19 -0500

    .9_dev: Auto stash before merge of "V0.9_dev" and "origin/V0.9_dev"

--- src/Controller/BorrowController.php
+++ src/Controller/BorrowController.php
@@ -335,18 +335,27 @@ class BorrowController extends AbstractController
 
             if ($book->getStatus() == $statusEnable) {
                 $book->setStatus($statusBorrowed);
+<<<<<<< Updated upstream
             
+=======
+        
+>>>>>>> Stashed changes
                 $this->em->persist($borrow);
                 $this->em->flush();
                 $this->em->persist($book);
                 $this->em->flush();
+<<<<<<< Updated upstream
                 return new JsonResponse($borrow->getIdBorrow());
+=======
+                $jsonBorrow['idBorrow']=$borrow->getIdBorrow();
+                return new JsonResponse($jsonBorrow);
+>>>>>>> Stashed changes
             }
             
 
             return new JsonResponse(['error' => 'cannot borrow'], 409);
         }
-        return new JsonResponse([]);    
+        return new JsonResponse(["final countDown"]);    
     }
 
     //--------------------------------
