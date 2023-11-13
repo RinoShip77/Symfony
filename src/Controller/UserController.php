c5e8f26e2cec52141e842e06b015a329fe3b7d9b
Olivier Bourgault <77986350+Olivier1875009@users.noreply.github.com>
2023-11-13 17:23:05 -0500

    update profile picture

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -107,12 +107,19 @@ class UserController extends AbstractController
 				if (strlen($uploadedFile) > 0) {
 					$newFilename = $idUser . ".png";
 
-					if (Tools::deleteImage($this->imagesDestinationDirectory,  $newFilename)) {
+					//Delete the previous image
+					if ($this->deleteImage($newFilename)) {
 						try {
 							$uploadedFile->move($this->imagesDestinationDirectory, $newFilename);
 						} catch (FileException $e) {
 							return $this->json('File upload failed: ' . $e->getMessage(), 500);
 						}
+					}	else {
+							try {
+									$uploadedFile->move($this->imagesDestinationDirectory, $newFilename);
+							} catch (FileException $e) {
+									return $this->json('File upload failed: ' . $e->getMessage(), 500);
+							}
 					}
 				}
 
@@ -248,5 +255,18 @@ class UserController extends AbstractController
 		$reservation = $connection->executeStatement("UPDATE users SET fees = 0 WHERE idUser = $idUser");
 
 		return $this->json($reservation);
+	}
+
+	function deleteImage($filename)
+	{
+		$imagePath = $this->imagesDirectory . $filename;
+
+		if (file_exists($imagePath)) {
+			// Supprime l'image
+			unlink($imagePath);
+
+			return true;
+		}
+		return false;
 	}
 }
