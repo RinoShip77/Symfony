103a612d9ddb566ce5ba20bcab0f3c9823333dbb
Olivier Bourgault <77986350+Olivier1875009@users.noreply.github.com>
2023-11-17 15:06:25 -0500

    Complete the update profile picture

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -22,8 +22,8 @@ header('Access-Control-Allow-Origin: *');
 class UserController extends AbstractController
 {
 	private $em = null;
-	private $imagesDirectory = "images/users/templates/Picture";
-	private $imagesDestinationDirectory = "images/users/";
+	private $imagesTemplateDirectory = "images/users/templates/Picture";
+	private $imagesDirectory = "images/users/";
 	private $imagesExtension = ".png";
 
 	//--------------------------------
@@ -107,15 +107,15 @@ class UserController extends AbstractController
 				if (strlen($uploadedFile) > 0) {
 					$newFilename = $idUser . "_" . $request->request->get('timestamp') . ".png";
 
-					//Delete the previous image
-						try {
-							$uploadedFile->move($this->imagesDestinationDirectory, $newFilename);
-						} catch (FileException $e) {
-							return $this->json('File upload failed: ' . $e->getMessage(), 500);
-						}
+					try {
+						$uploadedFile->move($this->imagesDirectory, $newFilename);
+						$connexion->executeStatement("UPDATE users SET profilePicture = '$newFilename' WHERE idUser = $idUser");
+					} catch (FileException $e) {
+						return $this->json('File upload failed: ' . $e->getMessage(), 500);
+					}
 				}
 
-				// copy($this->imagesDirectory . $request->request->get('pictureNumber') . $this->imagesExtension, $this->imagesDestinationDirectory . $idUser . $this->imagesExtension);
+				// copy($this->imagesTemplateDirectory . $request->request->get('pictureNumber') . $this->imagesExtension, $this->imagesDirectory . $idUser . $this->imagesExtension);
 				break;
 
 			case 'updatePassword':
